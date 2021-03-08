8ff49f407b63d6438ea4445e798a205ba7cd8f70
rahulbksc <hirahul.jiit@gmail.com>
2021-03-08 23:31:44 +0100

    Consider log level for each message

--- logger.cpp
+++ logger.cpp
@@ -1,79 +1,74 @@
-//Rahul Yadav 5th Sept 2018
-#pragma once
-
 #include "logger.hpp"
+#include <thread>
+#include <mutex>
+#include <utility>
 
 using namespace user;
 
-void logger::registerHandler(std::string name, callback c)
+void logger::RegisterHandler(std::string name, callback c)
 {
 	m_log_handlers.insert(std::pair<std::string, callback> {name, c });
 }
 
-void logger::deRegisterHandler(const std::string& name)
+void logger::DeRegisterHandler(const std::string& name)
 {
 	auto it = m_log_handlers.find(name);
 	if (it != m_log_handlers.end())
 		m_log_handlers.erase(name);
 }
 
-void logger::setLogLevel(log_level l)
+void logger::SetLogLevel(log_level l)
 {
-	m_level = l;
+	level_ = l;
 }
 
-void logger::debug(const std::string& msg)
+void logger::AddMessage(const std::string&  msg ,logger::log_level l)
 {
-	if (m_level >= log_level::debug)
+	std::lock_guard<std::mutex> lk{mutex_};
+	auto val = std::make_pair(msg,l);
+	buffer_.push_back(val);	
+	cv_.notify_one();
+}
+
+void logger::WriteMessage()
+{
+	std::unique_lock<std::mutex> lk{mutex_};
+	cv_.wait(lk,[this](){
+		return this->buffer_.size() != 0;
+	});
+
+	auto msg_with_level = buffer_.front();
+	buffer_.pop_front();
+
+	const std::string& msg  = msg_with_level.first;
+	logger::log_level l  = msg_with_level.second;
+
+	if (level_ >= l)
 	{
 		for (auto cb : m_log_handlers)
 		{
-			std::lock_guard<std::mutex> lock(m_mutex);
 			cb.second(msg);
 		}
 	}
+	
+}
+
+void logger::Debug(const std::string& msg)
+{
+	AddMessage(msg, log_level::debug);	
 };
 
-void logger::info(const std::string& msg)
+void logger::Info(const std::string& msg)
 {
-	if (m_level >= log_level::info)
-	{
-		for (auto cb : m_log_handlers)
-		{
-			std::lock_guard<std::mutex> lock(m_mutex);
-			cb.second(msg);
-		}
-	}
+	AddMessage(msg, log_level::info);
 }
 
-void logger::warn(const std::string& msg)
+void logger::Warn(const std::string& msg)
 {
-	if (m_level >= log_level::warn)
-	{
-		for (auto cb : m_log_handlers)
-		{
-			std::lock_guard<std::mutex> lock(m_mutex);
-			cb.second(msg);
-		}
-	}
+	AddMessage(msg, log_level::warn);
 }
 
-void logger::error(const std::string& msg)
+void logger::Error(const std::string& msg)
 {
-	if (m_level >= log_level::error)
-	{
-		for (auto cb : m_log_handlers)
-		{
-			std::lock_guard<std::mutex> lock(m_mutex);
-			cb.second(msg);
-		}
-
-	}
+	AddMessage(msg, log_level::error);
 }
-
-
-
-
-
-
-
